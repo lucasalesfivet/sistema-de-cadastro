dd6fff29a4c3b38e6f714c2a0265912b9d7f9fdc
Lucas Sales <marx.s.lukas@gmail.com>
2022-09-27 22:39:45 -0300

    tentativa de reverter

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
+<<<<<<< HEAD
 //mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula!");
 console.log("enviando para o github via vscode");
+=======
+>>>>>>> parent of 9adf8da (teste)
